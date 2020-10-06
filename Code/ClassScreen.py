a7598c59feca4640f954bc1ca22cbbde69812db4
sydthecoderkid <bookmasterdc@gmail.com>
2020-10-05 23:40:04 -0400

    working on text fade

--- Code/ClassScreen.py
+++ Code/ClassScreen.py
@@ -1,8 +1,21 @@
 import pyglet
+import FadeText
 from pyglet.window import key
 
 class ClassScreen:
+
+
+    def __init__(self, textlabel, alphacolor): 
+        self.textlabel = textlabel
+        self.alphacolor = alphacolor
+        fader = FadeText.FadeText(textlabel, alphacolor)
+        fader.fadeText(textlabel)
+
+
     
     @staticmethod
     def SelectClass(label):
-        label.text = "Hello"
+        #FadeText.fadeText(self, label)
+        label.text = "Hello Adventurer! "
+ 
+
--- Code/WelcomeScreen.py
+++ Code/WelcomeScreen.py
@@ -1,6 +1,9 @@
 import pyglet
 from pyglet.window import key
 from ClassScreen import ClassScreen
+import FadeText
+
+
 
 window = pyglet.window.Window()
 label = pyglet.text.Label("Welcome, adventurer. Let us begin your journey. Press any key to continue.", font_name = 'Times New Roman', font_size = 20, x = 75, y = 300)
@@ -13,8 +16,10 @@ def on_draw():
 
 @window.event
 def on_key_press(symbol, modifiers):
-   ClassScreen().SelectClass(label)
-
+   screen = ClassScreen(label, 255)
+   #screen.SelectClass(screen.textlabel)
+   
+   
 pyglet.app.run()
 
 
